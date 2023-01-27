46009902b12b5ad7653acffaac64f07054ee54ac
shiiiniii <115028625+shiiiniii@users.noreply.github.com>
2023-01-27 04:43:59 +0300

    Create model.py

---
+++ model.py
@@ -0,0 +1,13 @@
+db_list = []
+def read_db(path: str) -> list:
+    global db_list
+    with open(path, 'r', encoding='UTF-8') as file:
+        my_list = file.readlines()
+        for line in my_list:
+            id_dict = dict()
+            line = line.strip().split(';')
+            id_dict['lastname'] = line[0]
+            id_dict['firstname'] = line[1]
+            id_dict['phone'] = line[2]
+            id_dict['comment'] = line[3]
+            db_list.append(id_dict)
